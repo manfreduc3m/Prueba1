3af721e73e48ba7393afd733538ec895fe7c972f
JF Rascón Crespo <jfrascon@gmail.com>
2013-02-26 21:57:50 +0100

    Sigo haciendo pruebas con los commits

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -7,8 +7,7 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- 		System.out.println("Git is fun");
- 		boolean wow = false;
+ 		System.out.println("Git is cool");
 
 	}
 
